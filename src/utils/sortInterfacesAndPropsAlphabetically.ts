7d125984c896a8f48b5fb26bd1b722174a9b58c3
mateus <mateusrpinheiro.contato@outlook.com>
2023-03-21 20:27:21 -0300

    ignore all nodes after the function loader

--- src/utils/sortInterfacesAndPropsAlphabetically.ts
+++ src/utils/sortInterfacesAndPropsAlphabetically.ts
@@ -1,8 +1,8 @@
 import * as ts from 'typescript';
 
-export function sortInterfacesAndPropsAlphabetically(sourceCode: string): string {
+export function sortInterfacesAndPropsAlphabetically(sourceCode: string, fileName: string): string {
   const sourceFile = ts.createSourceFile(
-    'temp.ts',
+    fileName,
     sourceCode,
     ts.ScriptTarget.ESNext,
     true
@@ -12,25 +12,9 @@ export function sortInterfacesAndPropsAlphabetically(sourceCode: string): string
 
   const transformer: ts.TransformerFactory<ts.SourceFile> = (context) => {
     const visit: ts.Visitor = (node) => {
-      if (ts.isInterfaceDeclaration(node)) {
-        const sortedMembers = node.members.slice().sort((a, b) => {
-          if (ts.isPropertySignature(a) && ts.isPropertySignature(b)) {
-            const aName = a.name.getText(sourceFile);
-            const bName = b.name.getText(sourceFile);
-            return aName.localeCompare(bName);
-          }
-          return 0;
-        });
-
-        return ts.factory.updateInterfaceDeclaration(
-          node,
-          node.decorators,
-          node.modifiers,
-          node.name,
-          node.typeParameters,
-          node.heritageClauses,
-          sortedMembers
-        );
+      if (ts.isInterfaceDeclaration(node) || ts.isTypeAliasDeclaration(node)) {
+        const sortedMembers = sortTypeMembers(node, sourceFile);
+        return updateTypeNode(node, sortedMembers);
       }
 
       return ts.visitEachChild(node, visit, context);
@@ -39,10 +23,51 @@ export function sortInterfacesAndPropsAlphabetically(sourceCode: string): string
     return (sourceFile) => ts.visitNode(sourceFile, visit);
   };
 
-  //usar uma função de transformação e passar o contexto que é fornecido
-
   const result = ts.transform(sourceFile, [transformer]);
   const transformedSourceFile = result.transformed[0] as ts.SourceFile;
 
   return printer.printNode(ts.EmitHint.Unspecified, transformedSourceFile, sourceFile);
+}
+
+function sortTypeMembers(node: ts.InterfaceDeclaration | ts.TypeAliasDeclaration, sourceFile: ts.SourceFile) {
+  const members = ts.isInterfaceDeclaration(node) ? node.members : ts.isTypeLiteralNode(node.type) ? node.type.members : [];
+
+  const sortedMembersArray = members.slice().sort((a, b) => {
+    if (ts.isPropertySignature(a) && ts.isPropertySignature(b)) {
+      const aName = a.name.getText(sourceFile);
+      const bName = b.name.getText(sourceFile);
+      return aName.localeCompare(bName);
+    }
+    return 0;
+  });
+  // @ts-ignore
+  return ts.factory.createNodeArray(sortedMembersArray, members.hasTrailingComma);
+}
+
+function updateTypeNode(node: ts.InterfaceDeclaration | ts.TypeAliasDeclaration, sortedMembers: ts.NodeArray<ts.TypeElement>) {
+  if (ts.isInterfaceDeclaration(node)) {
+    return ts.factory.updateInterfaceDeclaration(
+      node,
+      node.decorators,
+      node.modifiers,
+      node.name,
+      node.typeParameters,
+      node.heritageClauses,
+      sortedMembers
+    );
+  } else if (ts.isTypeAliasDeclaration(node)) {
+    if (ts.isTypeLiteralNode(node.type)) {
+      const updatedType = ts.factory.updateTypeLiteralNode(node.type, sortedMembers);
+      return ts.factory.updateTypeAliasDeclaration(
+        node,
+        node.decorators,
+        node.modifiers,
+        node.name,
+        node.typeParameters,
+        updatedType
+      );
+    }
+  }
+
+  return node;
 }
