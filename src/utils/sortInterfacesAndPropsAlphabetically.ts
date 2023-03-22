aafd74e711bb5c9b4bfbf5ddb6339e697730884b
mateus <mateusrpinheiro.contato@outlook.com>
2023-03-21 21:04:02 -0300

    avoid jsx nodes

--- src/utils/sortInterfacesAndPropsAlphabetically.ts
+++ src/utils/sortInterfacesAndPropsAlphabetically.ts
@@ -7,11 +7,28 @@ export function sortInterfacesAndPropsAlphabetically(sourceCode: string, fileNam
     ts.ScriptTarget.ESNext,
     true
   );
-
-  const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+  const printer = ts.createPrinter({
+    newLine:
+      sourceFile.getLineEndOfPosition(sourceFile.getEnd()) === ts.NewLineKind.CarriageReturnLineFeed
+        ? ts.NewLineKind.CarriageReturnLineFeed
+        : ts.NewLineKind.LineFeed,
+  });
 
   const transformer: ts.TransformerFactory<ts.SourceFile> = (context) => {
     const visit: ts.Visitor = (node) => {
+      // Verifique se o nó atual é um arquivo de origem e se é um arquivo TypeScript ou TypeScript React
+      if (ts.isSourceFile(node)) {
+        if (!node.fileName.endsWith('.ts') && !node.fileName.endsWith('.tsx')) {
+          return node;
+        }
+      }
+
+      // Se o nó atual for uma função ou método, não visite os filhos
+      if (ts.isFunctionDeclaration(node) || ts.isMethodDeclaration(node)) {
+        return node;
+      }
+
+      // Verifique se o nó atual é um TypeAliasDeclaration ou InterfaceDeclaration
       if (ts.isInterfaceDeclaration(node) || ts.isTypeAliasDeclaration(node)) {
         const sortedMembers = sortTypeMembers(node, sourceFile);
         return updateTypeNode(node, sortedMembers);
@@ -26,7 +43,20 @@ export function sortInterfacesAndPropsAlphabetically(sourceCode: string, fileNam
   const result = ts.transform(sourceFile, [transformer]);
   const transformedSourceFile = result.transformed[0] as ts.SourceFile;
 
-  return printer.printNode(ts.EmitHint.Unspecified, transformedSourceFile, sourceFile);
+  return customJsxPrinter(printer, transformedSourceFile, sourceFile);
+}
+
+function customJsxPrinter(printer: ts.Printer, node: ts.Node, sourceFile: ts.SourceFile): string {
+  if (ts.isJsxElement(node)) {
+    const opening = printer.printNode(ts.EmitHint.Unspecified, node.openingElement, sourceFile);
+    const closing = printer.printNode(ts.EmitHint.Unspecified, node.closingElement, sourceFile);
+    const children = node.children.map((child) => printer.printNode(ts.EmitHint.Unspecified, child, sourceFile)).join('');
+    return opening + children + closing;
+  } else if (ts.isInterfaceDeclaration(node) && node.members.length === 0) {
+    const interfaceKeyword = printer.printNode(ts.EmitHint.Unspecified, node, sourceFile);
+    return `${interfaceKeyword} {}`;
+  }
+  return printer.printNode(ts.EmitHint.Unspecified, node, sourceFile);
 }
 
 function sortTypeMembers(node: ts.InterfaceDeclaration | ts.TypeAliasDeclaration, sourceFile: ts.SourceFile) {
@@ -44,7 +74,8 @@ function sortTypeMembers(node: ts.InterfaceDeclaration | ts.TypeAliasDeclaration
   return ts.factory.createNodeArray(sortedMembersArray, members.hasTrailingComma);
 }
 
-function updateTypeNode(node: ts.InterfaceDeclaration | ts.TypeAliasDeclaration, sortedMembers: ts.NodeArray<ts.TypeElement>) {
+function updateTypeNode(node: ts.InterfaceDeclaration | ts.TypeAliasDeclaration, sortedMembers:
+ ts.NodeArray<ts.TypeElement>) {
   if (ts.isInterfaceDeclaration(node)) {
     return ts.factory.updateInterfaceDeclaration(
       node,
